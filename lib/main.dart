4b1e88508c08e5620d49275fb6024c4af3516bbf
Daniil Ryzhkov <daniil.ryzhkov@sarix.eu>
2025-05-11 01:28:03 +0200

    Implemented address suggestions

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,13 @@ import 'package:intl/intl.dart';
 import 'package:reactive_forms/reactive_forms.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'dart:io' show Platform;
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'dart:async';
 
-void main() {
+void main() async {
+  await dotenv.load(); // Load environment variables
   Intl.defaultLocale = 'ru_RU'; // Set default locale explicitly
   runApp(const MyApp());
 }
@@ -125,74 +130,70 @@ class _WhatsAppLinkGeneratorState extends State<WhatsAppLinkGenerator> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Center(child: Text('БРИОН Встречи')),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: ReactiveForm(
-          formGroup: form,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              ReactiveValueListenableBuilder<DateTime>(
-                formControlName: 'date',
-                builder: (context, control, child) {
-                  return InkWell(
-                    onTap: () => _selectDateTime(context),
-                    child: InputDecorator(
-                      decoration: const InputDecoration(
-                        labelText: 'Дата',
-                        border: OutlineInputBorder(),
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus(); // Unfocus all fields when tapping outside
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Center(child: Text('БРИОН Встречи')),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: ReactiveForm(
+            formGroup: form,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ReactiveValueListenableBuilder<DateTime>(
+                  formControlName: 'date',
+                  builder: (context, control, child) {
+                    return InkWell(
+                      onTap: () => _selectDateTime(context),
+                      child: InputDecorator(
+                        decoration: const InputDecoration(
+                          labelText: 'Дата',
+                          border: OutlineInputBorder(),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          mainAxisSize: MainAxisSize.max,
+                          children: <Widget>[
+                            Text(_datetimeToString(control.value!)),
+                            const Icon(Icons.calendar_today),
+                          ],
+                        ),
                       ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        mainAxisSize: MainAxisSize.max,
-                        children: <Widget>[
-                          Text(_datetimeToString(control.value!)),
-                          const Icon(Icons.calendar_today),
-                        ],
-                      ),
-                    ),
-                  );
-                },
-              ),
-              const SizedBox(height: 16),
-              ReactiveTextField<String>(
-                formControlName: 'phoneNumber',
-                decoration: const InputDecoration(
-                  labelText: 'Номер телефона',
-                  border: OutlineInputBorder(),
+                    );
+                  },
                 ),
-                keyboardType: TextInputType.phone,
-                validationMessages: {
-                  ValidationMessage.required: (_) => 'Введите номер телефона',
-                },
-              ),
-              const SizedBox(height: 16),
-              ReactiveTextField<String>(
-                formControlName: 'address',
-                decoration: const InputDecoration(
-                  labelText: 'Адрес',
-                  border: OutlineInputBorder(),
+                const SizedBox(height: 16),
+                ReactiveTextField<String>(
+                  formControlName: 'phoneNumber',
+                  decoration: const InputDecoration(
+                    labelText: 'Номер телефона',
+                    border: OutlineInputBorder(),
+                  ),
+                  keyboardType: TextInputType.phone,
+                  validationMessages: {
+                    ValidationMessage.required: (_) => 'Введите номер телефона',
+                  },
                 ),
-                validationMessages: {
-                  ValidationMessage.required: (_) => 'Введите адрес',
-                },
-              ),
-              const SizedBox(height: 32),
-              ReactiveFormConsumer(
-                builder: (context, form, child) {
-                  return ElevatedButton(
-                    onPressed: form.valid || form.pristine
-                        ? _generateAndOpenWhatsAppLink
-                        : null,
-                    child: const Text('Отправить в WhatsApp'),
-                  );
-                },
-              ),
-            ],
+                const SizedBox(height: 16),
+                _AddressField(form: form),
+                const SizedBox(height: 32),
+                ReactiveFormConsumer(
+                  builder: (context, form, child) {
+                    return ElevatedButton(
+                      onPressed: form.valid || form.pristine
+                          ? _generateAndOpenWhatsAppLink
+                          : null,
+                      child: const Text('Отправить в WhatsApp'),
+                    );
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
@@ -202,5 +203,176 @@ class _WhatsAppLinkGeneratorState extends State<WhatsAppLinkGenerator> {
   String _datetimeToString(DateTime dateTime) {
     return '${DateFormat.yMd(Intl.defaultLocale).format(dateTime)} '
         '${DateFormat.jm(Intl.defaultLocale).format(dateTime)}';
+  }
+}
+
+class _AddressField extends StatefulWidget {
+  const _AddressField({required this.form});
+
+  final FormGroup form;
+
+  @override
+  State<_AddressField> createState() => _AddressFieldState();
+}
+
+class _AddressFieldState extends State<_AddressField> {
+  final FocusNode _addressFocusNode = FocusNode(); // FocusNode for address field
+  final LayerLink _layerLink = LayerLink(); // Link for overlay positioning
+  OverlayEntry? _overlayEntry;
+  bool _isAddressFocused = false;
+  List<String> _lastSuggestions = [];
+
+  final StreamController<List<String>> _suggestionsStreamController =
+      StreamController<List<String>>.broadcast();
+
+  Timer? _debounce; // Timer for debounce
+
+  @override
+  void initState() {
+    super.initState();
+    _addressFocusNode.addListener(() {
+      setState(() {
+        _isAddressFocused = _addressFocusNode.hasFocus;
+        if (_isAddressFocused) {
+          _showOverlay();
+        } else {
+          _removeOverlay();
+        }
+      });
+    });
+
+    widget.form.control('address').valueChanges.listen((query) {
+      if (_debounce?.isActive ?? false) _debounce?.cancel();
+      _debounce = Timer(const Duration(milliseconds: 300), () async {
+        final suggestions = await _fetchAddressSuggestions(query);
+        _lastSuggestions = suggestions;
+        _suggestionsStreamController.add(suggestions);
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _removeOverlay();
+    _addressFocusNode.dispose();
+    _suggestionsStreamController.close();
+    _debounce?.cancel(); // Cancel the debounce timer
+    super.dispose();
+  }
+
+  void _showOverlay() {
+    if (_overlayEntry != null) return;
+
+    _overlayEntry = OverlayEntry(
+      builder: (context) {
+        final theme = Theme.of(context);
+
+        return Positioned(
+          width: MediaQuery.of(context).size.width - 32, // Match field width
+          child: CompositedTransformFollower(
+            link: _layerLink,
+            showWhenUnlinked: false,
+            offset: const Offset(0, 56), // Position below the text field
+            child: Material(
+              type: MaterialType.card,
+              elevation: 4,
+              child: StreamBuilder<List<String>>(
+                initialData: _lastSuggestions,
+                stream: _suggestionsStreamController.stream,
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                    return const SizedBox.shrink();
+                  }
+
+                  return ConstrainedBox(
+                    constraints: const BoxConstraints(
+                      maxHeight: 200,
+                    ),
+                    child: ListView.builder(
+                      shrinkWrap: true,
+                      padding: EdgeInsets.zero,
+                      itemCount: snapshot.data!.length,
+                      itemBuilder: (context, index) {
+                        final suggestion = snapshot.data![index];
+                        return ListTile(
+                          title: Text(suggestion, style: theme.textTheme.bodyMedium),
+                          onTap: () {
+                            widget.form.control('address').updateValue(suggestion);
+                          },
+                        );
+                      },
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
+        );
+      },
+    );
+
+    Overlay.of(context).insert(_overlayEntry!);
+  }
+
+  void _removeOverlay() {
+    _overlayEntry?.remove();
+    _overlayEntry = null;
+  }
+
+  Future<List<String>> _fetchAddressSuggestions(String? query) async {
+    if (query == null) return [];
+    if (query.isEmpty) return [];
+    if (query.length < 3) return []; // Minimum length for suggestions
+    final apiKey = dotenv.env['YANDEX_API_KEY']; // Load API key from .env
+    final url = Uri.parse(
+        'https://suggest-maps.yandex.ru/v1/suggest?text=$query&lang=ru_RU&apikey=$apiKey&results=5');
+    try {
+      final response = await http.get(url);
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        final suggestions = List<String>.from(data['results'].map((item) {
+          final title = item['title']['text'];
+          final subtitle = item['subtitle']['text'];
+          return '$title, $subtitle';
+        }));
+        return suggestions;
+      }
+    } catch (e) {
+      debugPrint('Error fetching suggestions: $e');
+    }
+    return [];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CompositedTransformTarget(
+      link: _layerLink,
+      child: ReactiveTextField<String>(
+        formControlName: 'address',
+        focusNode: _addressFocusNode, // Attach the FocusNode
+        decoration: InputDecoration(
+          labelText: 'Адрес',
+          border: const OutlineInputBorder(),
+          suffixIcon: StreamBuilder(
+                stream: widget.form.control('address').valueChanges,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData && snapshot.data != null && snapshot.data!.isNotEmpty) {
+                    return IconButton(
+                      icon: const Icon(Icons.clear),
+                      onPressed: () {
+                        widget.form.control('address').reset(); // Clear the field
+                      },
+                    );
+                  }
+
+                  return const SizedBox.shrink(); // No icon if the field is empty
+                }
+              ),
+        ),
+        validationMessages: {
+          ValidationMessage.required: (_) => 'Введите адрес',
+        },
+      ),
+    );
   }
 }
