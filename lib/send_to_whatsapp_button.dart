58e2e0e9269204d7a4c0518101ea31b820ffc8cb
Daniil Ryzhkov <daniil.ryzhkov.97@gmail.com>
2025-05-12 20:33:00 +0200

    Adjusted message template

--- lib/send_to_whatsapp_button.dart
+++ lib/send_to_whatsapp_button.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 
-import 'package:brion_appointment/utils.dart';
 import 'package:flutter/material.dart';
 import 'package:reactive_forms/reactive_forms.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -31,15 +30,30 @@ class SendToWhatsappButton extends StatelessWidget {
       return;
     }
 
-    final dateTime = datetimeToString(form.control('date').value as DateTime);
+    final dateTime = form.control('date').value as DateTime;
+    final year = dateTime.year.toString();
+    final month = dateTime.month.toString().padLeft(2, '0');
+    final day = dateTime.day.toString().padLeft(2, '0');
+    final hour = dateTime.hour.toString().padLeft(2, '0');
+    final minute = dateTime.minute.toString().padLeft(2, '0');
     final phoneNumber = form.control('phoneNumber').value;
     final address = form.control('address').value;
 
-    final message = 'Ваша встреча:\n'
-        'Дата/время: $dateTime\n'
-        'Адрес: $address\n\n'
-        'Посетите наш сайт: https://brionestate.ru/#wa\n'
-        'Если ссылка не активна, добавьте этот номер в контакты.';
+    final message =
+        '*Запланированная встреча*\n'
+        '\n'
+        'Дата: _$day/$month/${year}_\n'
+        'Время: _$hour:${minute}_\n'
+        'Адрес: _${address}_\n'
+        '\n'
+        'В случае если у Вас изменятся планы, сообщите, пожалуйста, об этом здесь.\n'
+        '\n'
+        'Все предлагаемые объекты и услуги Вы можете увидеть, посетив наш сайт:\n'
+        'https://brionestate.ru/#wa\n'
+        '\n'
+        'Если ссылка не активна, добавьте, пожалуйста, этот номер в контакты.\n'
+        '\n'
+        '_С уважением, Елена_';
 
     final encodedMessage = Uri.encodeComponent(message);
     final whatsappUrl = Platform.isIOS
@@ -51,7 +65,7 @@ class SendToWhatsappButton extends StatelessWidget {
     if (await canLaunchUrl(url)) {
       await launchUrl(url);
       form.reset();
-      form.control('date').value = DateTime.now(); // Reset the date to now
+      form.control('date').value = DateTime.now();
     } else {
       messenger.showSnackBar(
         const SnackBar(content: Text('Ошибка составления ссылки WhatsApp')),
